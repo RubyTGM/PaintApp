9c85542a5c85aaf2e14029e98d156e69f2ab5079
TinSeungYuenKelvinAdrien <155611664+TinSeungYuenKelvinAdrien@users.noreply.github.com>
2024-01-06 09:04:13 +0400

    sketch.js

--- MyMediocrePaintApp/sketch.js
+++ MyMediocrePaintApp/sketch.js
@@ -30,6 +30,7 @@ const drawRect = (e) => {
     ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
 }
 const drawCircle = (e) => {
+    ctx.beginPath();
     let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX), 2) + Math.pow((prevMouseY - e.offsetY), 2));
     ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI); 
     fillColor.checked ? ctx.fill() : ctx.stroke();
